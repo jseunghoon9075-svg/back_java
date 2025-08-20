835e06572f6eef158ee7c7c79cd78125b6e4d8d0
jseunghoon9075-svg <jseunghoon9075@gmail.com>
2025-08-21 08:51:55 +0900

    JAVA 4일차 배열연습!

--- array/src/array/Practice04.java
+++ array/src/array/Practice04.java
@@ -68,17 +68,83 @@ public class Practice04 {
 //		금요일
 //		0 ~ 6 사이 숫자 입력 : 7 
 //		잘못 입력하셨습니다.
-		char[] week = {'월', '화', '수', '목', '금', '토', '일'};
-		int num1 = 0, num2 = 0;
-		
-		Scanner sc = new Scanner(System.in);
-		System.out.print("0 ~ 6 사이 숫자 입력 : ");
-		
-		num1 = sc.nextInt();
-		
-			if(num1 >= 0 && num1 < 7) {
-				System.out.print(week[num1]+"요일");
-			} else
-				System.out.println("잘 못 입력하셨습니다\n입력예시)2\n출력예시)수요일");
+//		char[] week = {'월', '화', '수', '목', '금', '토', '일'};
+//		int num1 = 0, num2 = 0;
+//		
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("0 ~ 6 사이 숫자 입력 : ");
+//		
+//		num1 = sc.nextInt();
+//		
+//			if(num1 >= 0 && num1 < 7) {
+//				System.out.print(week[num1]+"요일");
+//			} else
+//				System.out.println("잘 못 입력하셨습니다\n입력예시)2\n출력예시)수요일");
+//		사용자가 배열의 길이를 직접 입력하여 그 값만큼 정수형 배열을 선언 및 할당하고
+//		배열의 크기만큼 사용자가 직접 값을 입력하여 각각의 인덱스에 값을 초기화 하세요. 그리고
+//		배열 전체 값을 나열하고 각 인덱스에 저장된 값들의 합을 출력하세요
+//		ex.
+//		정수 : 5
+//		배열 0번째 인덱스에 넣을 값 : 4
+//		배열 1번째 인덱스에 넣을 값 : -4
+//		배열 2번째 인덱스에 넣을 값 : 3
+//		배열 3번째 인덱스에 넣을 값 : -3
+//		배열 4번째 인덱스에 넣을 값 : 2
+//		4 -4 3 -3 2
+//		총 합 : 2
+//		Scanner sc = new Scanner(System.in);
+//		int arrSize = 0, count = 0;
+//		
+//		System.out.println("선언할 배열의 크기 : \nEX) 2 -> 2칸 배열생성");
+//		arrSize = sc.nextInt();
+//		
+//		int[] arr = new int[arrSize];
+//		
+//		for(int i = 0; i < arr.length; i++) {
+//			System.out.printf("배열 %d번째 인덱스에 넣을 값 : ", i );
+//			arr[i] = sc.nextInt();
+//		}
+//		
+//		for(int i = 0; i < arr.length; i++) {
+//			System.out.print("인덱스 값:" + arr[i] + " ");
+//			count += arr[i];
+//		}
+//		System.out.printf("인덱스값의 총 합 : %d", count);
+
+//		3이상인 홀수 자연수를 입력 받아 배열의 중간까지는 1부터 1씩 증가하여 오름차순으로 값을 넣고,
+
+//		중간 이후부터 끝까지는 1씩 감소하여 내림차순으로 값을 넣어 출력하세요. 단, 
+//		입력한 정수가 홀수가 아니거나 3 미만일 경우 “다시 입력하세요”를 출력하고 다시 정수를 받도록 하세요.
+		//
+//		ex. 정수 : 4
+//		다시 입력하세요.
+//		정수 : -6
+//		다시 입력하세요.
+//		정수 : 5
+//		1 2 3 2 1
+//		Scanner sc = new Scanner(System.in);
+//		int num = 0, count = 0;
+//		System.out.println("3이상의 홀수 자연수를 입력하세요 : ");
+//		num = sc.nextInt();
+//		if ((num >= 3) && (num % 2 == 1)) {
+//			int[] arr = new int[num];
+//
+//			for (int i = 0; i < arr.length; i++) {
+//
+//				if (i <= arr.length / 2) {
+//					count++;
+//					arr[i] = count;
+//				} else {
+//					count--;
+//					arr[i] = count;
+//				}
+//			}
+//			for (int i = 0; i < arr.length; i++) {
+//				System.out.print(arr[i] + " ");
+//			}
+//		} else {
+//			System.out.println("3이상의 홀수를 입력해주세요");
+//		}
+//		sc.close();
 	}
 }
