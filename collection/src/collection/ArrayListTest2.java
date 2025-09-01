84a588f574124bc29891189bc4f0702f14b57543
jseunghoon9075-svg <jseunghoon9075@gmail.com>
2025-09-02 00:55:05 +0900

    JAVA 12일차 복습git add .

--- collection/src/collection/ArrayListTest2.java
+++ collection/src/collection/ArrayListTest2.java
@@ -1,24 +1,36 @@
 package collection;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class ArrayListTest2 {
+	
+	public  boolean addArray(ArrayList<Integer> datas, int num) {
+		return datas.add(num);
+	}
+	
+	public int setArray(ArrayList<Integer> datas, int index, int value) {
+		return datas.set(datas.indexOf(index), value);
+	}
+	
+	public void removeArray(ArrayList<Integer> datas, int value) {
+		if(datas.contains(value)) {
+			datas.remove(datas.get(datas.indexOf(value)));
+		}
+	}
+	
 	public static void main(String[] args) {
 		ArrayList<Integer> datas = new ArrayList<Integer>();
-		System.out.println(datas.size());
-		datas.add(10);
-		datas.add(20);
-		datas.add(40);
-		datas.add(30);
-		datas.add(50);
-		datas.add(60);
-		datas.add(10);
-		datas.add(90);
-		datas.add(80);
+		ArrayListTest2 arrList = new ArrayListTest2();
+		arrList.addArray(datas, 300);
+		for(int i = 0; i < 2000; i += 100) {
+			arrList.addArray(datas, i);
+		}
 		
 		System.out.println(datas);
 		System.out.println(datas.size());
-		System.out.println(datas.get(0));
 
 		
 //		실습
@@ -27,17 +39,40 @@ public class ArrayListTest2 {
 //		수정90을 9로 수정
 //		삭제
 //		80을 삭제
-		if(datas.contains(50))
+//			datas.contains() boolean 타입의 값을 리턴해준다.
+		try {
+			if(datas.contains(0)) {
+//			만약에 50이라는 값이 있다면 true면 
+//			.add 값을 넣어주는데(인덱스 , 요소)를 넣어준다 값을 넣어 줄 인덱스 앞의 인덱스에
+//				+ 1을 하면 항상 그 인덱스 뒤에 값이 들어가게 된다.
+				datas.add(datas.indexOf(0) + 1, 500);
+			}
+		}catch(IndexOutOfBoundsException e) {
+		System.out.println("잘못된 인덱스를 입력하셨습니다!");		
+		} catch (Exception e) {
+			e.printStackTrace();
+			e.getMessage();
+		}
+		
+		arrList.setArray(datas, 1500, 10);
 		
 		try {
-			datas.add(5, 500);
-			datas.set(8, 9);
-			datas.remove(9);
-		} catch (IndexOutOfBoundsException e) {
-			System.out.println("인덱스 범위에 벗어났습니다");
-		} catch(Exception e) {
-			e.getMessage();
+			if(datas.contains(90)) {
+//			.set 값을 수정하는데 사용된다. .set(인덱스, 요소) 예외가 발생할 수 있으므로 예외처리를 같이 해주면좋다
+				datas.set(datas.indexOf(90), 9);
+			}
+		}catch(IndexOutOfBoundsException e) {
+			System.out.println("잘못된 인덱스를 입력하셨습니다!");		
+		} catch (Exception e) {
 			e.printStackTrace();
+			e.getMessage();
 		}
-	}
-}
+
+		arrList.removeArray(datas, 300);
+		if(datas.contains(80)) {
+			datas.remove(datas.get(datas.indexOf(80)));
+		}
+		System.out.println(datas);
+		
+
+	}}
--- lambda/src/lambdaExpert/MyMath.java
+++ lambda/src/lambdaExpert/MyMath.java
@@ -41,59 +41,46 @@ public class MyMath {
 	}
 
 	public static void main(String[] args) {
-
+		ArrayList<String> strArr = new ArrayList<>();
+		
 		OperCheck operCheck = expression -> {
 			String[] input = null;
 //			문자열을 입력받고 1~9까지의 문자열을 제외하고 배열로 반환(연산자 분리)
 				input = expression.split("[0-9]|[0-9]");
-//						
 			return input;
 		};
-
 		Scanner sc = new Scanner(System.in);
 //    입력받은 문자열
 		String msg = sc.next();
 		String[] numArr = null, result = null;
-		int resultArr = 0;
-		int num1 = 0, num2 = 0;
+		int num1 = 0;
 //		입력받은 문자열 중 숫자만분리
 		numArr = msg.split("\\+|\\-");
+		int[] num2 = new int[numArr.length - 1];
 //		입력받은 문자열 중 연산자분리 (문자열배열로 반환받음)
 		result = operCheck.getOpers(msg);
 		
 		for(String a: numArr) {
 			System.out.println("입력받은 문자열 중 정수:"+a);
 		}
+		System.out.println("길이" + numArr.length);
 		
 		num1 = Integer.parseInt(numArr[0]);
-		
-		for(int i = 0; i < result.length + i; i++) {
-			System.out.println("공백배열 제거 시도"+ result[i]);
-			if((result[i].equals(null))) {
-				result[i].trim();
-			}else if(!(result[i].equals(null))) {
-//				연산자 전달
-				Calc calc = calculater(result[i]);
-//				넘버 전달
-				System.out.println(calc.calc(num1, Integer.parseInt(numArr[i + 1])));
-				System.out.println("작동");
-				return;
+		for(int i = 0; i < result.length; i++) {
+			if(!(result[i].equals(""))) {
+				strArr.add(result[i]);
 			}
-			
 		}
-//		for(String p: result) {
-//			System.out.println("입력받은 문자열 중 정수:"+p);
-//		}
-//		for(int i = 0; i < result.length; i++) {
-////			만약에 연산자 분리한 배열값이 null이 아니면 리절트에있는 값을 리절트어레이에 넣는다 연산자가 있으면 넣는다이거지
-//			if(!(result[i].equals(null))) {
-//				resultArr = result[i];
-//			}
-//			System.out.println("연산자 분리 후 빈배열 제거시도확인: ");
-//		}
-//		for(int i = 1; i < numArr.length; i++) {
-//			System.out.println(calc.calc(num1, Integer.parseInt(numArr[i])));
-//		}
-//		System.out.println("Calc에 넘긴값: " + calc.calc(-10, 60));
+		for(int i = 0; i < numArr.length; i++) {
+			if(i == 0) {
+				num2[i] = Integer.parseInt(numArr[i + 1]);
+			} else
+				continue;
+			System.out.println("num2[] 값: " + num2[i]);
+		}
+		for(int i = 0; i <numArr.length -1; i++) {
+			System.out.println(calculater(strArr.get(i)));
+		}
+		System.out.println(strArr);
 	}
 }
